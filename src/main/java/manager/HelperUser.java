62e4c17756ff98a5b84c1200743c342cfa247033
Katerina-09 <firiall68@gmail.com>
2025-02-06 10:46:00 +0200

    Added 06

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,4 +1,33 @@
 package manager;
 
-public class HelperUser {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HelperUser extends HelperBase{
+    public HelperUser(WebDriver wd) {
+        super(wd);
+    }
+    public void openLogForm(){
+        click(By.xpath("//a[contains(@href, '/login')]"));
+
+    }
+    public void fillLogForm(String email, String password){
+        type(By.id("email"), email);
+        type(By.id("password"), password);
+
+    }
+    public void submitLog(){
+        click(By.cssSelector("button[type = 'submit']"));
+
+    }
+    public void logOk(){
+        click(By.xpath("//button[text()='Ok']"));
+    }
+    public boolean isLogged(){
+        return isElementPresent(By.cssSelector("a.navigation-link[href='Logout?url=%2Fsearch']"));
+    }
+    public void logOut(){
+        click(By.cssSelector("a.navigation-link[href='Logout?url=%2Fsearch']"));
+
+    }
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,4 +1,31 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class LoginTests extends TestBase{
+    @BeforeMethod
+    public void preCondition(){
+        //if SingOut present ===>logout
+        if(app.getHelperUser().isLogged())
+            app.getHelperUser().logOut();
+
+    }
+    @Test
+    public void loginSuccess(){
+
+        app.getHelperUser().openLogForm();
+        app.getHelperUser().fillLogForm("firiall@mail.ru","Tele2user1!");
+        app.getHelperUser().submitLog();
+        //Assert
+//        Assert.assertEquals();
+//        Assert.assertNotEquals();
+//        Assert.assertTrue();
+//        Assert.assertFalse();
+        Assert.assertTrue(app.getHelperUser().isLogged());
+
+
+    }
+
 }
